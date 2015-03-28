0a64818a8e2be7113b0c9334303041ad244441fe
CelloCodez <thecellocoder@gmail.com>
2015-03-27 19:50:42 -0500

    Unneeded library and formatting
    Removed slick-util as it is not used, and formatted two components and
    removed unnecessary imports from them.

--- src/com/base/game/SineFloatComponent.java
+++ src/com/base/game/SineFloatComponent.java
@@ -19,30 +19,31 @@ package com.base.game;
 import com.base.engine.components.GameComponent;
 import com.base.engine.core.Time;
 import com.base.engine.core.Vector3f;
-import com.base.engine.physics.PhysicsEngine;
 import com.base.engine.rendering.RenderingEngine;
 import com.base.engine.rendering.Shader;
 
 public class SineFloatComponent extends GameComponent {
-	
+
 	private RenderingEngine m_renderingEngine;
-	
+
 	private Vector3f m_old = null;
-	
+
 	@Override
 	public void Update(float delta) {
 		if (m_renderingEngine != null) {
 			if (m_old == null) {
 				m_old = GetTransform().GetPos();
 			} else {
-				GetTransform().SetPos(m_old.Add(new Vector3f(0f, (float) Math.sin(Time.GetTime()) * 30f * delta, 0f)));
+				GetTransform().SetPos(
+						m_old.Add(new Vector3f(0f, (float) Math.sin(Time
+								.GetTime()) * 30f * delta, 0f)));
 			}
 		}
 	}
-	
+
 	@Override
 	public void Render(Shader shader, RenderingEngine renderingEngine) {
 		this.m_renderingEngine = renderingEngine;
 	}
-	
+
 }
